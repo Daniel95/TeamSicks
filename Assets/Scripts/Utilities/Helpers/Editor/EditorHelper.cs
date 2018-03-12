4c773bf7b3ccdcbc691dbdf7c4da3778de9e4e7a
Daniel95 <idaniel95@me.com>
2018-03-12 14:12:45 +0100

    added helpers, removed test scene

---
+++ Assets/Scripts/Utilities/Helpers/Editor/EditorHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class EditorHelper {
+
+    [MenuItem("Tools/Delete PlayerPrefs", priority = 800)]
+    public static void DeletePlayerPrefs() {
+        PlayerPrefs.DeleteAll();
+    }
+
+    [MenuItem("Tools/Go to Persistant Data path folder", priority = 800)]
+    public static void OpenPersistantDataPathFolder() {
+        EditorUtility.RevealInFinder(Application.persistentDataPath);
+    }
+
+    [MenuItem("Tools/Go to Data path folder", priority = 800)]
+    public static void OpenDataPathFolder() {
+        EditorUtility.RevealInFinder(Application.dataPath);
+    }
+
+    [MenuItem("Tools/Capture Background To Filter", priority = 800)]
+    public static void VisualiseRealsenseTouchPositions() {
+        if (RealsenseWrapperNLR.Instance == null) { return; }
+        RealsenseWrapperNLR.Instance.StartCaptureBackgroundToFilter();
+    }
+
+    [MenuItem("Tools/Remove Debug Objects", priority = 800)]
+    public static void RemoveDebugObjects() {
+        DebugHelper.RemoveAllDebugPositions();
+    }
+
+}

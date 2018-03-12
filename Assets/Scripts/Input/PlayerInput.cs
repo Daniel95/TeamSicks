56f78f17e9c08935b7a091099a9e424612297eff
Daniel95 <idaniel95@me.com>
2018-03-12 13:41:24 +0100

    added start of playerinput

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public static PlayerInput Instance { get { return GetInstance(); } }
+
+    private static PlayerInput instance;
+
+    public void EnableInputs(bool _enable)
+    {
+        if (_enable)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private static PlayerInput GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<PlayerInput>();
+        }
+        return instance;
+    }
+
+}

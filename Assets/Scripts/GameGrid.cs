ac5da13e7aea6ce9163da3e533e75e8cf74b8a56
Daniel95 <idaniel95@me.com>
2018-03-12 12:25:23 +0100

    added some example scripts

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameGrid : MonoBehaviour {
+
+    public static GameGrid Instance { get { return GetInstance(); } }
+
+    public Dictionary<Vector2, Node> NodeByGridPosition { get { return nodeByGridPosition; } }
+
+    private static GameGrid instance;
+
+    private Dictionary<Vector2, Node> nodeByGridPosition = new Dictionary<Vector2, Node>();
+
+    private static GameGrid GetInstance() {
+        if (instance == null) {
+            instance = FindObjectOfType<GameGrid>();
+        }
+        return instance;
+    }
+
+}
+
+public class Node {
+
+
+}
+
+public class LevelExample {
+
+    public int[,] testGrid = new int[5, 5]
+    {
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 2, 2, 0, 0 },
+        { 0, 2, 2, 0, 0 },
+        { 0, 0, 0, 0, 0 }
+    };
+
+    public void Test() {
+        NodeType nodeType = (NodeType)testGrid[2, 2];
+    }
+
+}
+
+public enum NodeType {
+    Empty = 0,
+    Player = 1,
+    Obstacle = 2,
+}

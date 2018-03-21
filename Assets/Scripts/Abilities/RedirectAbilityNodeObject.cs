42e90f85a0e82afd0210f6625e16c67d2dec938f
Daniel95 <idaniel95@me.com>
2018-03-21 16:13:17 +0100

    started redirectAbilityNodeObject

---
+++ Assets/Scripts/Abilities/RedirectAbilityNodeObject.cs
@@ -0,0 +1,21 @@
+﻿public class RedirectAbilityNodeObject : NodeObject
+{
+
+    private void OnNodeObjectAdded(NodeObjectType _nodeObjectType)
+    {
+        if(_nodeObjectType != NodeObjectType.Enemy) { return; }
+
+
+    }
+
+    private void OnEnable()
+    {
+        ParentNode.NodeObjectAddedEvent += OnNodeObjectAdded;
+    }
+
+    private void OnDisable()
+    {
+        ParentNode.NodeObjectAddedEvent -= OnNodeObjectAdded;
+    }
+
+}

77518448f2c700b674d9435d90ec90df6036e2d5
Daniel95 <idaniel95@me.com>
2018-03-27 16:46:31 +0200

    Win the game when turnsleft reaches 0. Calculate turnsleft start value based on the path the enemy needs to move

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    private void OnReachedEndpoint()
+    {
+        Debug.Log("LOSE");
+    }
+
+    private void OnTurnsOverEvent()
+    {
+        Debug.Log("WIN");
+    }
+
+    private void OnEnable()
+    {
+        DisplayTurnCounter.TurnsOverEvent += OnTurnsOverEvent;
+        EnemyNodeObject.ReachedEndpointEvent += OnReachedEndpoint;
+    }
+
+    private void OnDisable()
+    {
+        DisplayTurnCounter.TurnsOverEvent -= OnTurnsOverEvent;
+        EnemyNodeObject.ReachedEndpointEvent -= OnReachedEndpoint;
+    }
+
+}

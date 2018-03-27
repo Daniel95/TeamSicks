70639f91d2b560e8ca125a304a8a008f67abcc9b
tommtom9 <tommtom9@live.nl>
2018-03-27 14:10:52 +0200

    Updated menu to be UI instead of sprites Updated menu art Put menu in the build settings

---
+++ Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public void Load()
+    {
+        //We use the old function because the scenemanager doesnt function properly on mobile
+        SceneManager.LoadScene("Main");
+    }
+}
